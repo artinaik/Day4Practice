0bfc9c90045eb570edf7928401e55a8d9c183943
Your UserName <aratinaik151997@gmail.com>
2021-11-20 17:21:47 +0530

    Feature added to count number of die and position after every die

--- SnakeAndLadder/SnakeAndLadder/SnakeAndLadder.cs
+++ SnakeAndLadder/SnakeAndLadder/SnakeAndLadder.cs
@@ -8,39 +8,52 @@ namespace SnakeAndLadder
 {  
     class SnakeAndLadder
     {
-        public const int playerStartPosition = 0,playerWinPosition=100;
-        public static int nextPosition = 0;
-        public const int noPlay = 1, ladder = 2, snake = 3;
+        
         public void ChecksForOption()
         {
+
             Random randam = new Random();//for die
             Random random1 = new Random();//for switch case
-            while(nextPosition!=playerWinPosition)
+             const int playerStartPosition = 0, playerWinPosition = 100;
+              int nextPosition = 0;
+             const int noPlay = 1, ladder = 2, snake = 3;
+            int count = 0; 
+            while(nextPosition<playerWinPosition)
             {
+                
                 int answer = randam.Next(1, 7);//random variable for die
                 int choice = random1.Next(1, 4);//random variable for choice for player option
 
                 switch (choice)
                 {
                     case noPlay:
-                        nextPosition = playerStartPosition + nextPosition;//no play but nextposition is calculated
+                         //nextPosition = playerStartPosition + nextPosition;//no play but nextposition is calculated
+                        Console.WriteLine("[No Play] Position of player is " + nextPosition);
                         break;
                     case ladder:
-                        nextPosition = nextPosition + answer;
-                        nextPosition++;//next position increament by random die
+                        int newPosition = nextPosition + answer;
+                        //nextPosition = nextPosition + answer;
+                        if (newPosition > playerWinPosition)
+                            newPosition = nextPosition;
+                        else
+                            nextPosition = newPosition;
+                        //nextPosition++;//next position increament by random die
+                        Console.WriteLine("[Ladder]  Position of player " +nextPosition);
                         break;
                     case snake:
-                        int lastPosition = nextPosition - answer;//decramented by number of die
-                        if (lastPosition > 0)// checking if position greater than 0
-                            nextPosition = lastPosition;//assigned decreamented value to next position
+                        newPosition = nextPosition - answer;//decramented by number of die
+                        if (newPosition > 0)// checking if position greater than 0
+                            nextPosition = newPosition;//assigned decreamented value to next position
                         else
                             nextPosition = 0;
+                        Console.WriteLine("[Snake] Position of player is "+nextPosition);
                         break;
                 }
+                count++;
+
             }
-            //Console.WriteLine("Player won the game and position is "+nextPosition);//final win position is 100 
-            if (nextPosition == playerWinPosition)
-                Console.WriteLine("Player has got winning position  " + nextPosition);
+            Console.WriteLine("\nNumber of time dice was played is " + count);
+            Console.WriteLine("\nPlayer won the game and position is "+nextPosition);//final win position is 100 
         }
 
     }
